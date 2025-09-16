2e16084176aac43b52c8d6698bb78cb03f4ed1e4
Pavel Zikmund <xmzikmu01@vutbr.cz>
2025-09-16 22:36:30 +0200

    cL, cD and D calculation added

--- flight_analysis.py
+++ flight_analysis.py
@@ -11,6 +11,11 @@ class FlightPerformance:
 
         # Preallocate speeds as a 2D array [nh x nv]
         self.speeds = np.zeros((nh, nv))
+        self.cL = np.zeros((nh, nv))
+        self.cD = np.zeros((nh, nv))
+        self.D = np.zeros((nh, nv))
+
+        ar = adata.bref ** 2 / adata.sref
 
         for i, altitude in enumerate(self.altitudes):
             vstall = np.sqrt(
@@ -19,4 +24,12 @@ class FlightPerformance:
             )
             self.speeds[i, :] = np.linspace(vstall, vmax / 3.6, nv)
 
-        print(self.speeds)
+        for i, altitude in enumerate(self.altitudes):
+            rho = air_density(altitude)
+            for j, speed in enumerate(self.speeds[i, :]):
+                self.cL[i, j] = 2 * adata.mtow * g / (rho * adata.sref * speed ** 2)
+                self.cD[i, j] = adata.cd0 + self.cL[i,j] ** 2 / (np.pi * ar * adata.e)
+                self.D[i, j] = self.cD[i, j] * 0.5 * adata.sref * rho * speed ** 2
+
+        print(self.cD)
+        print(self.D)
