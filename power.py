61ce1b267281a046b130e84a6bde2f99b134b1ea
Pavel Zikmund <xmzikmu01@vutbr.cz>
2025-09-23 22:03:42 +0200

    turboprop altitude characteristics added

--- power.py
+++ power.py
@@ -2,11 +2,17 @@ from constants import rho0
 
 def power_available(rho, speed, adata):
     """
-    Calculate power required at a given air density (rho, kg/m3) and speed (m/s).
+    Calculate power available at a given air density (rho, kg/m3) and speed (m/s).
     Uses universal propeller efficiency characteristics and altitude characteristics.
     """
-    # altitude characteristics
-    pmaxh = adata.pmax * (1.132 * rho / rho0 - 0.132)
+    if adata.engtype == 1:
+        # altitude characteristics op reciprocating engine
+        pmaxh = adata.pmax * (1.132 * rho / rho0 - 0.132)
+    elif adata.engtype == 2:
+        # altitude characteristics op turboprop engine
+        pmaxh = adata.pmax * (rho / rho0) ** 0.7
+    else:
+        raise ValueError("Unknown engine type: {}".format(adata.engtype))
 
     # propeller efficiency
     L = speed / adata.vnom * 3.6
